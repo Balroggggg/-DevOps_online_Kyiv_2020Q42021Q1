c15b7e95c4135f0846fa63a8af4bede2b24af007
Balroggggg <48160928+Balroggggg@users.noreply.github.com>
2021-02-09 22:03:35 +0200

    Add files via upload

---
+++ m7/task7.1/keyscript.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if  [[ ! -n "$1" ]]
+then
+	echo "Key list:"
+	echo "--all - displays the IP addresses and symbolic names of all hosts in the current subnet"
+	echo "--target - key displays a list of open system TCP ports"
+fi
+
+function showIPandSymbNamesInSubnet {
+        echo "IP address        Host"
+        cat /etc/hosts | grep '^[0-2]'
+}
+
+function listOpenTcpPorts {
+	ss -tlwn | grep LISTEN
+}
+
+
+while [ -n "$1" ]
+do
+case "$1" in
+--all) showIPandSymbNamesInSubnet ;;
+--target) listOpenTcpPorts ;;
+*) echo "$1 is not a key";;
+esac
+shift
+done
+
+
+
+
